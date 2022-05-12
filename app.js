ec00c2c0f2c0efc3089d9f67528130dded530583
Chong Wen Hao <58220142+8kdesign@users.noreply.github.com>
2022-05-12 15:01:49 +0800

    Deployed to Heroku!!

--- app.js
+++ app.js
@@ -16,23 +16,6 @@ const app = express();
 var port_number = app.listen(process.env.PORT || 3000);
 app.listen(port_number);
 
-// app.use(bodyParser.json());
-
-// async function initialize() {
-// 	try {
-// 		const response = await axios.get(
-// 			`${TELEGRAM_API}/setwebhook?url=${WEBHOOK_URL}`
-// 		);
-// 		console.log(response.data);
-// 	} catch (error) {
-// 		console.log(error);
-// 	}
-// }
-// app.listen(process.env.PORT || 5000, async () => {
-// 	console.log("Running on port");
-// 	await initialize();
-// });
-
 // Bot
 
 const bot = new Telegraf("5202467843:AAEw8IbW7tn6teXzDOcJaoFb9r_He1DwTQM");
--- logic/menu.js
+++ logic/menu.js
@@ -27,5 +27,8 @@ export function initializeMenu({ bot }) {
 				}
 			);
 		});
+		if (data.answer.length > 0) {
+			bot.telegram.sendMessage(context.chat.id, data.answer);
+		}
 	});
 }
