e5881200453f20a98ca79a132fb92723613b7196
Chong Wen Hao <58220142+8kdesign@users.noreply.github.com>
2022-05-12 14:35:06 +0800

    Deployed to Heroku!!

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 import { Telegraf } from "telegraf";
+// import express from "express";
+// import bodyParser from "body-parser";
+// import axios from "axios";
 import { printMenu } from "./logic/main_menu.js";
 import { initializeMenu } from "./logic/menu.js";
 
+const { TOKEN, SERVER_URL } = process.env;
+const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
+const URI = `/webhook/${TOKEN}`;
+const WEBHOOK_URL = SERVER_URL + URI;
+
+// For Heroku
+
+// const app = express();
+// app.use(bodyParser.json());
+
+// async function initialize() {
+// 	try {
+// 		const response = await axios.get(
+// 			`${TELEGRAM_API}/setwebhook?url=${WEBHOOK_URL}`
+// 		);
+// 		console.log(response.data);
+// 	} catch (error) {
+// 		console.log(error);
+// 	}
+// }
+// app.listen(process.env.PORT || 5000, async () => {
+// 	console.log("Running on port");
+// 	await initialize();
+// });
+
+// Bot
+
 const bot = new Telegraf("5202467843:AAEw8IbW7tn6teXzDOcJaoFb9r_He1DwTQM");
-if (process.env.NODE_ENV === "production") {
-	bot.setWebHook(process.env.HEROKU_URL + bot.token);
-}
 
 bot.command("start", (context) => {
 	printMenu({ bot, context });
