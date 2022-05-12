f8fe71469a41df2393894e429128b3698fb72d19
Chong Wen Hao <58220142+8kdesign@users.noreply.github.com>
2022-05-12 13:54:59 +0800

    Deployed to Heroku!!

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import { printMenu } from "./logic/main_menu.js";
 import { initializeMenu } from "./logic/menu.js";
 
 const bot = new Telegraf("5202467843:AAEw8IbW7tn6teXzDOcJaoFb9r_He1DwTQM");
+if (process.env.NODE_ENV === "production") {
+	bot.setWebHook(process.env.HEROKU_URL + bot.token);
+}
 
 bot.command("start", (context) => {
 	printMenu({ bot, context });
@@ -11,17 +14,3 @@ bot.command("start", (context) => {
 initializeMenu({ bot });
 
 bot.launch();
-
-// const express = require("express");
-// const bodyParser = require("body-parser");
-
-// const app = express();
-
-// app.use(bodyParser.json());
-
-// app.listen(process.env.PORT);
-
-// app.post("/" + bot.token, (req, res) => {
-// 	bot.processUpdate(req.body);
-// 	res.sendStatus(200);
-// });
