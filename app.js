f162885740bddbe922392f0bdebf3d8b3d3e08b4
Chong Wen Hao <58220142+8kdesign@users.noreply.github.com>
2022-05-12 13:42:40 +0800

    Deployed to Heroku!!

--- app.js
+++ app.js
@@ -12,16 +12,16 @@ initializeMenu({ bot });
 
 bot.launch();
 
-const express = require("express");
-const bodyParser = require("body-parser");
+// const express = require("express");
+// const bodyParser = require("body-parser");
 
-const app = express();
+// const app = express();
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
-app.listen(process.env.PORT);
+// app.listen(process.env.PORT);
 
-app.post("/" + bot.token, (req, res) => {
-	bot.processUpdate(req.body);
-	res.sendStatus(200);
-});
+// app.post("/" + bot.token, (req, res) => {
+// 	bot.processUpdate(req.body);
+// 	res.sendStatus(200);
+// });
