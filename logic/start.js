027c8260bff6ad1782c525c3b7102cef3c8f2118
Chong Wen Hao <58220142+8kdesign@users.noreply.github.com>
2022-06-17 21:33:35 +0800

    Update start.js

--- logic/start.js
+++ logic/start.js
@@ -1,7 +1,7 @@
 export function printStart({ bot, context }) {
 	return new Promise((resolve) => {
 		const intro =
-			"Hello! Welcome to Spatial. Click on the menu button below to see all commands. Alternatively, tap on the following commands:\n\n/location - View all Spatial locations.\n\n/faq - View frequently asked questions.";
+			"Hello! Welcome to Spatial. Click on the menu button below to see all commands. Alternatively, tap on the following commands:\n\n/location - View all Spatial locations.\n/faq - View frequently asked questions.";
 		bot.telegram.sendMessage(context.chat.id, intro).then((message) => {
 			context.lastSentId = message.message_id;
 			resolve();
